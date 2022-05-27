72239b3abbd6a4448e97f28552a890833066a8aa
Razanleo <razan.i.omer@gmail.com>
2022-05-27 04:23:32 +0200

    loading & message sent

--- assets/js/verify.js
+++ assets/js/verify.js
@@ -1,4 +1,11 @@
-$('#gform').on('submit', function(e) {
-    document.getElementById('sent-message').style.display = "block";
-    document.getElementById("gform").reset();
-    });
+$('#gform').on('submit', function(e) 
+{
+    document.getElementById('loading').style.display = "block";
+
+    setTimeout(function(){
+            document.getElementById("gform").reset()
+            document.getElementById('loading').style.display = "none";
+            document.getElementById('sent-message').style.display = "block";
+        ;}, 2000);
+    
+});
